37582b45b4c054340447e1d030c4d7fe21032a7f
Damien Bose <damienbose3@gmail.com>
2024-08-06 20:18:10 +0530

    Finish part 2 of day 1

--- day1/main.py
+++ day1/main.py
@@ -1,19 +1,40 @@
-def getCodeForLine(input_line):
-    n = len(input_line)
-    c1 = None
-    for i in range(n):
-        c = input_line[i]
-        if c.isdigit():
-            c1 = c
-            break
-    c2 = None
-    for i in range(n - 1, -1, -1):
-        c = input_line[i]
-        if c.isdigit():
-            c2 = c
-            break
-    assert c1 is not None and c2 is not None, "Invalid Number of digits"
-    return int(c1 + c2)
+"""
+twolnine - iterate and find the first substring which matches our pars - iterate and find the first substring which matches our parserr
+
+for reverse, we reverse the solution matches and do the same thing
+"""
+
+matches = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+numbers = [str(num) for num in range(1, 10)]
+
+
+def getForwardCode(input_line):
+    for i in range(len(input_line)):
+        for match in matches:
+            if input_line[i:].startswith(match):
+                return str(matches[match])
+            elif input_line[i].isdigit():
+                return input_line[i]
+
+
+def getBackwardCode(input_line):
+    for i in range(len(input_line) - 1, -1, -1):
+        for match in matches:
+            if input_line[i:].startswith(match):
+                return str(matches[match])
+            elif input_line[i].isdigit():
+                return input_line[i]
 
 
 def parseFile(path):
@@ -21,8 +42,10 @@ def parseFile(path):
     with open(path, "r") as file:
         lines = file.readlines()
         for line in lines:
-            code = getCodeForLine(line)
-            cum += code
+            f_code = getForwardCode(line)
+            b_code = getBackwardCode(line)
+            assert f_code is not None and b_code is not None
+            cum += int(f_code + b_code)
     print(cum)
 
 
